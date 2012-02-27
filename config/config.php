5244757605d74bfe53a4c72c70618f5a5e334fc1
Taylor Otwell <taylorotwell@gmail.com>
2012-02-27 15:51:56 -0600

    added config option for extension.
    Signed-off-by: Taylor Otwell <taylorotwell@gmail.com>

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+
+	'extension' => '.twig.php',
+
+);
--- start.php
+++ start.php
@@ -1,55 +1,67 @@
 <?php
 
 /**
- * Get the path to the Twig compilation directory.
+ * Push the bootstrap until after application start to give time to set configs
  */
-$cache = path('storage').'cache/twig';
-
-/**
- * Make the cache directory if it isn't there
- */
-if ( ! is_dir($cache)) mkdir($cache);
-
-/**
- * Register the Twig library with the auto-loader
- */
-Laravel\Autoloader::underscored(array(
-
-	'Twig' => Bundle::path('twig').'lib/Twig')
-
-);
-
-/**
- * Register the Twig extension as a valid View extension
- */
-Laravel\View::$extensions[] = '.twig.php';
-
-/**
- * Create and register the Twig instance in the IoC container
- */
-$loader = new Twig_Loader_Filesystem(array());
-
-$twig = new Twig_Environment($loader, compact('cache'));
-
-Laravel\IoC::instance('twig', $twig);
-
-/**
- * Hook into the Laravel view engine
- */
-Laravel\Event::listen(Laravel\View::engine, function($view) use ($twig)
+Event::listen('laravel.started: twig', function()
 {
-	// Only handle views that have the Twig extension
-	if ( ! str_contains($view->path, '.twig.php'))
+
+	/**
+	 * Grab the Twig file extension.
+	 */
+	$ext = Config::get('twig::config.extension');
+
+	/**
+	 * Get the path to the Twig compilation directory.
+	 */
+	$cache = path('storage').'cache/twig';
+
+	/**
+	 * Make the cache directory if it isn't there
+	 */
+	if ( ! is_dir($cache)) mkdir($cache);
+
+	/**
+	 * Register the Twig library with the auto-loader
+	 */
+	Laravel\Autoloader::underscored(array(
+
+		'Twig' => Bundle::path('twig').'lib/Twig')
+
+	);
+
+	/**
+	 * Register the Twig extension as a valid View extension
+	 */
+	Laravel\View::$extensions[] = $ext;
+
+	Laravel\View::$extensions = array_unique(Laravel\View::$extensions);
+
+	/**
+	 * Create and register the Twig instance in the IoC container
+	 */
+	$loader = new Twig_Loader_Filesystem(array());
+
+	$twig = new Twig_Environment($loader, compact('cache'));
+
+	Laravel\IoC::instance('twig', $twig);
+
+	/**
+	 * Hook into the Laravel view engine
+	 */
+	Laravel\Event::listen(Laravel\View::engine, function($view) use ($twig, $ext)
 	{
-		return false;
-	}
+		// Only handle views that have the Twig extension
+		if ( ! str_contains($view->path, $ext)) return false;
 
-	// Set the loader path to the correct path for the view
-	$twig->getLoader()->setPaths(array(dirname($view->path)));
+		// Set the loader path to the correct path for the view
+		$twig->getLoader()->setPaths(array(dirname($view->path)));
 
-	// Give one last chance to tweak the twig and view instance
-	Laravel\Event::fire('twig::rendering', array($twig, $view));
+		// Give one last chance to tweak the twig and view instance
+		Laravel\Event::fire('twig::rendering', array($twig, $view));
 
-	// Render back the template contents
-	return $twig->render(basename($view->path), $view->data());
+		// Render back the template contents
+		return $twig->render(basename($view->path), $view->data());
+	});
+
 });
